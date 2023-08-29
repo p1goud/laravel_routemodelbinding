a7917daab3d129b7644f6f2563d2b2164017931c
Pawan <pwngoud@gmail.com>
2023-08-29 17:14:13 +0530

    update

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
+
 class TransactionController extends Controller
 {
     public function index()
     {
-        $transactions = \App\Models\Transaction::with('user')->get();
+        $transactions = Transaction::with('user')->get();
 
         return view('transactions.index', compact('transactions'));
     }
 
     public function show(Transaction $transaction)
-    {
+    {   
         return view('transactions.show', compact('transaction'));
     }
 
     public function export(Transaction $transaction)
-    {
+    {   
         return view('transactions.export', compact('transaction'));
     }
 
     public function duplicate(Transaction $transaction)
     {
+        
         return view('transactions.duplicate', compact('transaction'));
     }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -11,7 +12,7 @@ class Transaction extends Model
 
     protected $fillable = ['uuid', 'amount', 'description', 'user_id'];
 
-    public function user()
+    public function user():BelongsTo
     {
         return $this->belongsTo(User::class);
     }
